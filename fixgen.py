06db26bcaae99b8ef40101469915dce9c9a78c4f
Mike Williams <mrmrdubya@gmail.com>
2010-03-03 14:34:33 +0000

    Fix fixture list offseting. Code optimisations.

--- fixgen.py
+++ fixgen.py
@@ -1,5 +1,19 @@
 #! /usr/local/bin/python
 
+# Copyright (c) 2010 Michael Williams <mrw@eandem.co.uk>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 # Generate team fixture list using a Berger table.
 #
 # One property of Berger table is that teams offset by half number of teams are
@@ -21,8 +35,6 @@ def generate(number=6, full=True, offset=0):
         number += 1
     # Generate initial team id sequence
     ids = [chr(ord('A') + i) for i in range(number)]
-    if offset:
-        ids = rotate(ids, offset)
     # Generate pairings - 1 less than number of teams
     mid_point = number/2
     pairings = []
@@ -32,24 +44,38 @@ def generate(number=6, full=True, offset=0):
         away.reverse()
         pairings.append(zip(home, away))
         ids = rotate(ids, 1)
-    # Interleave
-    interleaved = []
+    # Interleave and fix up last team home and away (always first in pairings)
+    fixtures = []
     for i in range(mid_point - 1):
-        interleaved.append(pairings[i])
-        interleaved.append(pairings[mid_point + i])
-    interleaved.append(pairings[mid_point - 1])
-    # Fix up last team home and aways - swap teams every other match
-    for i in range(1, number - 1, 2):
-        i = interleaved[i]
-        i[0] = i[0][1], i[0][0]
-    # Generate full home-away season
+        fixtures.append(pairings[i])
+        p = pairings[mid_point + i]
+        p[0] = p[0][1], p[0][0]
+        fixtures.append(p)
+    fixtures.append(pairings[mid_point - 1])
     if full:
-        interleaved[-2], interleaved[-1] = interleaved[-1], interleaved[-2]
-        t = []
-        for f in interleaved:
-            t.append([(a, h) for (h, a) in f])
-        interleaved.extend(t)
-    return interleaved
+        # Full season, swap last two fixture lists and then add reverse fixtures
+        fixtures[-2], fixtures[-1] = fixtures[-1], fixtures[-2]
+        fixtures.extend([[(away, home) for (home, away) in fixture]
+                            for fixture in fixtures])
+    # Shift starting point in fixtures
+    offset = offset % number
+    fixtures = fixtures[offset:] + fixtures[:offset]
+    return fixtures
 
-for f in generate():
-    print f
+fixtures = generate(10, offset=5)
+#for f in fixtures: print f
+
+tm = {}
+for f in fixtures:
+    for h, a in f:
+        hm = tm.get(h, [])
+        hm.append(a)
+        tm[h] = hm
+        am = tm.get(a, [])
+        am.append(' ')
+        tm[a] = am
+
+t = tm.keys()
+t.sort()
+for i in t:
+    print i, tm[i]
