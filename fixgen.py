68a69a5aaff375f0ccad1797bef8d6fd3b3fe920
Mike Williams <mrmrdubya@gmail.com>
2010-03-03 17:18:32 +0000

    Add command line command.  Tidy up generator module.

--- fixgen.py
+++ fixgen.py
@@ -14,10 +14,67 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-from fixture_generator import FixtureLists, berger_table
+"""Generate a fixture list.
 
-fl = FixtureLists(8, berger_table)
-fl.print_lists()
-fl.print_grid()
+usage: fixgen [-d] [-h] [-p number] number
+
+-d generate double round-robin (second half matches reversed).
+-h display this help.
+-p phase shift for fixture list.
+number of teams (odd values rounded up 1).
+"""
+
+import getopt
+import re
+import sys
+
+from fixture_generator import generate_fixtures, berger_table, print_lists, print_grid
+
+# Utility product information
+__product__ = 'Fixture generator'
+__copyright__ = 'Copyright 2010 Mike Williams. All rights reserved.'
+version_info = (0, 1, 0, 'beta', 0)
+__version__ = '%d.%d.%d' % version_info[:3]
+if version_info[3] != 'final':
+    __version__ += ' %s.%d' % version_info[3:]
+
+__description__ = '%s %s.\n%s' % (__product__, __version__, __copyright__)
+
+USAGE = re.search('^usage:.*', __doc__, re.MULTILINE).group()
+
+def usage(mesg):
+    sys.exit('%s\n%s' % (mesg, USAGE))
+
+# Parse the command line options
+try:
+    opts, pargs = getopt.getopt(sys.argv[1:], 'dhp:')
+except getopt.GetoptError, e:
+    usage(e)
+
+double = False
+offset = 0
+for option, value in opts:
+    if option == '-h':  # Display help
+        sys.exit(__doc__)
+
+    elif option == '-d':  # Double round robin
+        double = True
+
+    elif option == '-p':  # Offset starting fixture
+        try:
+            offset = int(value)
+        except:
+            usage('Invalid start fixture shift.')
+
+if len(pargs) != 1:
+    usage('Wrong number of arguments given.')
+try:
+    number = int(pargs[0])
+except:
+    usage('Invalid number of teams')
+
+fl = generate_fixtures(number, berger_table, double, offset)
+print_lists(fl)
+print_grid(fl)
 
 #eof fixgen.py
